c045f2a594ad50e1bdccc4695a1aeb58fc541408
felipe <felipe@Felipe>
2017-10-11 17:51:10 -0300

    declaracion y print matrices

---
+++ PrimerosPasos/src/Uso_arrays.java
@@ -0,0 +1,24 @@
+
+public class Uso_arrays {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		/*int mi_matriz[] = new int [5];
+		
+		mi_matriz [0] = 5;
+		mi_matriz [1] = 38;
+		mi_matriz [2] = -15;
+		mi_matriz [3] = 92;
+		mi_matriz [4] = 71;*/
+		
+		int [] mi_matriz = {5,38,-15,92,71,95,85,65,24,14,78,65,12,45,78,95};
+		
+		for(int i = 0; i<mi_matriz.length; i++){
+			
+			System.out.println(mi_matriz[i]);
+		}
+
+	}
+
+}
