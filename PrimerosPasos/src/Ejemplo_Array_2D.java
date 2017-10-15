2ec248fddfa31edd826b378be1ba2a37fd1d874e
felipe <felipe@Felipe>
2017-10-15 02:07:08 -0300

    matriz llenado-impreso automatico

---
+++ PrimerosPasos/src/Ejemplo_Array_2D.java
@@ -0,0 +1,37 @@
+
+public class Ejemplo_Array_2D {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		double acumulado;
+		double interes= 0.10;
+		
+		double [][] saldo = new double [6][5];
+		
+		for(int i=0; i<6; i++){
+			
+			saldo[i][0]=10000;
+			acumulado=10000;
+			
+			for(int j=1;j<5;j++){
+				
+				acumulado = acumulado+(acumulado*interes);
+				saldo[i][j]=acumulado;
+			}
+			
+			interes=interes+0.01;
+		}
+		
+		for(double []elemento:saldo){
+			
+			System.out.println("");
+			for(double z: elemento){
+				
+				System.out.print(z+" ");
+			}
+		}
+
+	}
+
+}
