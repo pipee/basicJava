74e0a14d74faba672f1f097ceb16728ec1a4fd59
felipe <felipe@Felipe>
2017-10-16 03:02:10 -0300

    Creacion objeto básico

---
+++ PrimerosPasos/src/poo/Uso_Auto.java
@@ -0,0 +1,13 @@
+package poo;
+
+public class Uso_Auto {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Auto Suzuki = new Auto();
+		
+		System.out.println("Este auto tiene "+Suzuki.ruedas+" ruedas");
+	}
+
+}
