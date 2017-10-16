70c012f3324e811bd5c3157b6f77843a254d43bc
felipe <felipe@Felipe>
2017-10-16 03:01:44 -0300

    Creación clase auto

---
+++ PrimerosPasos/src/poo/Auto.java
@@ -0,0 +1,19 @@
+package poo;
+
+public class Auto {
+	
+	int ruedas;
+	int largo;
+	int ancho;
+	int motor;
+	int peso;
+	
+	public Auto (){
+		
+		ruedas=4;
+		largo=2000;
+		ancho=300;
+		motor=1600;
+		peso=500;
+	}
+}
