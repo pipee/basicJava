58c9cc6c146a4489cc684f9f7e2bbd10bc1c0ef1
felipe <felipe@Felipe>
2017-10-26 02:05:54 -0300

    getter setter de un auto

--- PrimerosPasos/src/poo/Auto.java
+++ PrimerosPasos/src/poo/Auto.java
@@ -6,11 +6,11 @@ public class Auto {
 	private int largo;
 	private int ancho;
 	private int motor;
-	private int peso_carroseria;
+	private int peso_plataforma;
 	
-	String color;
-	int peso;
-	boolean asientos_cuero, climatizador;
+	private String color;
+	private int peso_total;
+	private boolean asientos_cuero, climatizador;
 	
 	public Auto (){
 		
@@ -18,12 +18,14 @@ public class Auto {
 		largo=2000;
 		ancho=300;
 		motor=1600;
-		peso_carroseria=500;
+		peso_plataforma=500;
 	}
 	
-	public String dime_largo(){ //getter
+	public String dime_datos_generales(){ //getter
 		
-		return "El largo es " + largo;
+		return "La plataforma del auto tiene " + ruedas+" ruedas"+ " Mide "+ largo/1000+ " metros "+
+		"Con un ancho de "+ancho+" cm "+" Con un motor de "+motor+" CC "+
+		" y un peso de carrosería de "+peso_plataforma+" kg";
 	}
 	
 	public String dime_color(){
@@ -31,8 +33,65 @@ public class Auto {
 		return "El color del auto es "+color;
 	}
 	
-	public void establece_color(){ //setter
+	public void establece_color(String color_auto){ //setter
 		
-		color="blanco";
+		color=color_auto;
+	}
+	
+	public void configura_asientos(String asientos_cuero){//setter
+		
+		if (asientos_cuero.equalsIgnoreCase("si")){
+			
+			this.asientos_cuero=true;
+		}else{
+			this.asientos_cuero=false;
+		}
+	}
+	public String dime_asientos(){//getter
+		
+		if(asientos_cuero==true){
+			return "El auto tiene asientos de cuero ";
+		}else{
+			return "El auto tiene asientos de serie ";
+		}
+	}
+	public void configura_climatizador(String climatizador){
+		
+		if(climatizador.equalsIgnoreCase("si")){
+			this.climatizador=true;
+		}else{
+			this.climatizador=false;
+		}
+	}
+	public String dime_climatizador(){
+		if(climatizador==true){
+			return "El auto tiene climatizador";
+		}else{
+			return "El auto tiene A/C";
+		}
+	}
+	public String dime_peso_coche(){//getter/setter
+		int peso_carroceria=500;
+		
+		peso_total=peso_plataforma+peso_carroceria;
+		
+		if(asientos_cuero==true){
+			peso_total=peso_total+50;
+		}
+		if(climatizador==true){
+			peso_total=peso_total+20;
+		}
+		return "El peso del auto es "+peso_total+" Kilos";
+	}
+	public int precio_auto(){//getter
+		int precio_final=10000;
+		
+		if(asientos_cuero==true){
+			precio_final+=2000;
+		}
+		if(climatizador==true){
+			precio_final+=1500;
+		}
+		return precio_final;
 	}
 }
--- PrimerosPasos/src/poo/Uso_Auto.java
+++ PrimerosPasos/src/poo/Uso_Auto.java
@@ -1,5 +1,7 @@
 package poo;
 
+import javax.swing.*;
+
 public class Uso_Auto {
 
 	public static void main(String[] args) {
@@ -7,9 +9,20 @@ public class Uso_Auto {
 		
 		Auto Suzuki = new Auto();
 		
-		//Suzuki.establece_color();
+		Suzuki.establece_color(JOptionPane.showInputDialog("Ingresa color"));
 		
+		System.out.println(Suzuki.dime_datos_generales());
 		System.out.println(Suzuki.dime_color());
+		
+		Suzuki.configura_asientos(JOptionPane.showInputDialog("Asientos de cuero?"));
+		System.out.println(Suzuki.dime_asientos());
+		
+		Suzuki.configura_climatizador(JOptionPane.showInputDialog("Climatizador?"));
+		System.out.println(Suzuki.dime_climatizador());
+		
+		System.out.println(Suzuki.dime_peso_coche());
+		
+		System.out.println("El precio final del auto es "+Suzuki.precio_auto()+" Euros");
 	}
 
 }
